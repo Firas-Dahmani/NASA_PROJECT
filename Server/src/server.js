ab079fddcb4ea7cb6f37d2e830847a3f315bdb10
Firas Dahmani <firas1dahmani@gmail.com>
2023-03-16 21:16:31 +0100

    add data base

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,19 +1,24 @@
-const app = require('./app');
-
 const http = require('http');
 
-const { loadPlanetData } = require('./models/planets.model')
+require('dotenv').config();
 
-const PORT = process.env.PORT || 8000
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
-const server = http.createServer(app)
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
 
 async function startServer() {
-  await loadPlanetData()
-
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
   server.listen(PORT, () => {
-    console.log("Listening on port " + PORT + " ...");
-  })
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
 
-startServer()
+startServer();
